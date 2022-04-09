4f8e8018f02c013b81112b4b0fafcf7bb6c64ce8
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2022-04-08 20:04:37 -0600

    ok
    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      
+      <v-col>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bienvenido!!
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Front End del sistema de tareas,<br> para utilizar como ejemplo en las practicas de despliegue.
+          
+        </p>
+      </v-col>
+
+      <v-col cols="12">
+      
+        <h2 class="headline font-weight-bold mb-3">
+          Agregar:
+        </h2>
+
+        <v-row justify="center">
+
+         <v-col cols="4">
+          <v-text-field
+            label="Nueva Tarea:"
+            outlined
+          ></v-text-field>
+      </v-col> 
+      <v-col cols="1" class="mt-2">
+        <v-btn color="primary" >Agregar</v-btn>
+      </v-col>
+
+          
+
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Tareas
+        </h2>
+        <v-row justify="center" >
+          <v-card v-for="tarea in tareas" 
+          :key="tarea.id"
+          max-width="400"
+          color="grey lighten-2"
+          class="ma-3"
+          >            
+              <v-col >
+                <v-checkbox
+                  v-model="tarea.hecho"
+                  :label="tarea.nombre"
+                ></v-checkbox>
+                <v-col>
+                  <v-btn
+                    fab
+                    dark
+                    small
+                    color="primary"
+                  >
+                    <v-icon dark>
+                      mdi-minus
+                    </v-icon>
+                  </v-btn>
+                </v-col>              
+                
+              </v-col>
+          </v-card>
+          
+          </v-row>
+        
+      </v-col>
+
+      
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      tareas:[
+            {
+              "id": "624f90bad9069d158165b821",
+              "nombre": "lavar los trastes",
+              "hecho": false
+            },
+            {
+              "id": "624f90ccd9069d158165b822",
+              "nombre": "limpiar el cuarto",
+              "hecho": true
+            },
+            {
+              "id": "624f90ccd9069d158165b824",
+              "nombre": "limpiar el cuarto",
+              "hecho": true
+            }
+          ]
+    }),
+    methods:()=>({
+      cargarDatos(){
+        this.axios.get('http://localhost:3000/tareas')
+              .then(function(response){
+                console.log(response)
+              })
+
+      },
+    })
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')

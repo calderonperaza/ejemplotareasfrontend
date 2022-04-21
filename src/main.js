b955112dae3926624c11b359428568bb939caaeb
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2022-04-20 22:09:10 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VueAxios from 'vue-axios'
 
 //Al correr en contenedor usara la variable de entorno backend
 //Para referir el service name
-axios.defaults.baseURL= process.env.BACKEND || "http://backend:3000"
+axios.defaults.baseURL= process.env.BACKEND || "http://localhost:3000"
 
 
 Vue.use(VueAxios, axios)

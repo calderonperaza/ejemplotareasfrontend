07646a12d0dc6e8213149b9ab4e8476c82f1f5e8
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2022-05-09 21:51:01 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import '@babel/polyfill'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-
+//url del backend recuerda quitar cualquier pleca del final / quitarla
 axios.defaults.baseURL= process.env.BACKEND || "http://backend.alexandercalderon.me:3000"
 
 Vue.use(VueAxios, axios)

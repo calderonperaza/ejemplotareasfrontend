9358d29e0eae232b85d7dad35bf25c587659f996
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2022-05-18 11:47:27 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-//axios.defaults.baseURL= process.env.BACKEND || "http://backend.alexandercalderon.me:3000"
-axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
+axios.defaults.baseURL= process.env.BACKEND || "http://backend.alexandercalderon.me:3000"
+//axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
 
 
 Vue.use(VueAxios, axios)

7cf442b82c9f41892e08f2fc28c6b184aaf181fd
Enrique Trujillo <lt11003@ues.edu.sv>
2022-06-22 14:01:46 -0600

    ok

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-axios.defaults.baseURL= process.env.BACKEND || "https://uesejemplotarea1.us-south.cf.appdomain.cloud/"
+axios.defaults.baseURL= process.env.BACKEND || "https://uesejemplotarea1.us-south.cf.appdomain.cloud"
 //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
 
 

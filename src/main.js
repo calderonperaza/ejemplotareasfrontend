8afaa5669821bfe8b9465fbae03ac082a97d0cd6
Enrique Trujillo <lt11003@ues.edu.sv>
2022-06-21 22:38:12 -0600

    ok

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+//url del backend recuerda quitar cualquier pleca del final / quitarla
+axios.defaults.baseURL= process.env.BACKEND || "https://uesejemplotarea1.us-south.cf.appdomain.cloud/"
+//axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
+
+
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')

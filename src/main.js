cf7b2d6d557d948cce90a1b0e311dbf3f0d8abcf
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2023-07-01 09:04:26 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import VueAxios from 'vue-axios'
 //url del backend recuerda quitar cualquier pleca del final / quitarla
 //axios.defaults.baseURL= process.env.BACKEND || "https://backend.alexandercalderon.me"
 //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
-axios.defaults.baseURL= process.env.BACKEND || "http://alexandercalderon.online"
+axios.defaults.baseURL= process.env.BACKEND || "http://localhost:3000"
 
 
 Vue.use(VueAxios, axios)

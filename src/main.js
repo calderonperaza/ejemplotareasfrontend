43dd213723aa99ee79faf3478a77352139405bee
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2024-03-18 16:57:53 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-//axios.defaults.baseURL= process.env.BACKEND || "https://backend.alexandercalderon.me"
-//axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
-axios.defaults.baseURL= process.env.BACKEND || "http://localhost:3000"
-
+axios.defaults.baseURL= process.env.VUE_APP_URL_BACKEND || "http://localhost:3000"
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
